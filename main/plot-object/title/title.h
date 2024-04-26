ff446a11c892c780e985537df92f4a4a2c3b1bd7
duxxen <a.romanovu@gmail.com>
2024-04-27 01:46:27 +0300

    Added: Title class

--- main/plot-object/title/title.h
+++ main/plot-object/title/title.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "../plot-object.hpp"
+#include "../../style/text-style.hpp"
+
+namespace Plotter
+{
+	class Title : public PlotObject
+	{
+		friend class Axis;
+		friend class Cursor;
+		friend class Grid;
+		friend class Plot;
+
+	public:
+		Title(Plot* layout, TextStyle style);
+
+	private:
+		TextStyle	style;	
+	};
+}
